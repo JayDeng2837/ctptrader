774e211edaced0467832fe7a16aeac4864f8dc6e
aaa <aa@163.com>
2016-07-20 10:55:18 +0800

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@
 import sys
 import os
 
+from recommonmark.parser import CommonMarkParser
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -36,6 +38,11 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
 source_suffix = ['.rst', '.md']
 #source_suffix = '.rst'
 
